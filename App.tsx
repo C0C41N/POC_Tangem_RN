2ea6a0e6f311e955254244d9abf11ac92185c69c
C0C41N <51693606+C0C41N@users.noreply.github.com>
2024-11-05 21:27:30 +0500

    chore: minor

--- App.tsx
+++ App.tsx
@@ -20,13 +20,16 @@ function App(): React.JSX.Element {
 
   const scanPressed = async () => {
     try {
+
       const result = await scan({
         msgHeader: 'Scan Card',
         msgBody: 'Please scan your card',
         accessCode: '141414',
       });
+
       console.log(JSON.stringify(result.backupStatus, null, 2));
       console.log(JSON.stringify(result.wallets.map(e => ({curve: e.curve, publicKey: e.publicKeyBase58})), null, 2));
+
     } catch (error) {
       console.log(error);
     }
@@ -34,8 +37,10 @@ function App(): React.JSX.Element {
 
   const signPressed = async () => {
     try {
+
       const unsignedHex = '01000103c6dadd07fa6b967f95c1c794207a6660b6c103bb3d9225cb65a32aec9233bd4a7851663184f478288effadd0b24e403c625569350166ae9dfc10e1eaf4a203b000000000000000000000000000000000000000000000000000000000000000003aab9ecdda6344c5dea7dc04242579a2171d7e0b7659ac1d16b20ab1f00ba77901020200010c020000001027000000000000';
       const pubKeyBase58 = '2MfvVtqxsER27y7uCJmiVncoLPM7XFHeQ3oJWLwQRMdF';
+
       const result = await sign({
         pubKeyBase58,
         unsignedHex,
@@ -43,7 +48,9 @@ function App(): React.JSX.Element {
         msgHeader: 'Sign Transaction',
         msgBody: 'Please scan card to sign the transaction',
       });
+
       console.log(result);
+
     } catch (error) {
       console.log(error);
     }
@@ -51,15 +58,18 @@ function App(): React.JSX.Element {
 
   const purgeAllWalletsPressed = async () => {
     try {
+
       const result = await purgeAllWallets({
         accessCode: '141414',
         msgHeader: 'Purge Wallets',
         msgBody: 'Please scan card to purge all wallets',
       });
+
       console.log(JSON.stringify({
         length: result.length,
         wallets: result.map(e => ({curve: e.curve, publicKey: e.publicKey})),
       }, null, 2));
+
     } catch (error) {
       console.log(error);
     }
@@ -67,15 +77,18 @@ function App(): React.JSX.Element {
 
   const createAllWalletsPressed = async () => {
     try {
+
       const result = await createAllWallets({
         accessCode: '141414',
         msgHeader: 'Create Wallets',
         msgBody: 'Please scan card to create all wallets',
       });
+
       console.log(JSON.stringify({
         length: result.length,
         wallets: result.map(e => ({curve: e.curve, publicKey: e.publicKey})),
       }, null, 2));
+
     } catch (error) {
       console.log(error);
     }
@@ -83,12 +96,14 @@ function App(): React.JSX.Element {
 
   const setAccessCodePressed = async () => {
     try {
+
       await setAccessCode({
         accessCode: '000000',
         newAccessCode: '141414',
         msgHeader: 'Set Access Code',
         msgBody: 'Please scan card to set access code',
       });
+
     } catch (error) {
       console.log(error);
     }
@@ -132,10 +147,12 @@ function App(): React.JSX.Element {
 
   const resetBackupPressed = async () => {
     try {
+
       await resetBackup({
         accessCode: '141414',
         msgBody: 'Reset Backup',
       });
+
     } catch (error) {
       console.log(error);
     }

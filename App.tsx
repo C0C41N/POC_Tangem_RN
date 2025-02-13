26375ba85ccbc6791b4a91469261fa95810b9894
C0C41N <51693606+C0C41N@users.noreply.github.com>
2025-02-13 16:09:04 +0500

    chore: minor

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {SafeAreaView, StatusBar, Button, View, ViewStyle} from 'react-native';
 import {useColorScheme} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {scan, createAllWallets, purgeAllWallets, resetCard, signMultiple } from 'tangem-sdk-codora-react-native';
+import {scan, purgeAllWallets, validateMnemonic, enableUserCodeRecovery } from 'tangem-sdk-codora-react-native';
 
 import { install } from 'react-native-quick-crypto';
 
@@ -32,11 +32,9 @@ function App(): React.JSX.Element {
       const pubKey = wallet.publicKeyBase58;
       console.log(`Wallet | ${curve} | ${pubKey}`);
     });
-  };
 
-  const createAllWalletsPressed = async () => {
-    const resp = await createAllWallets({});
-    console.log(resp);
+    const isUserCodeRecoveryAllowed = card.data?.userSettings.isUserCodeRecoveryAllowed;
+    console.log({ isUserCodeRecoveryAllowed });
   };
 
   const purgeAllWalletsPressed = async (onlyEd25519?: boolean) => {
@@ -44,19 +42,14 @@ function App(): React.JSX.Element {
     console.log(resp);
   };
 
-  const resetCardPressed = async () => {
-    const resp = await resetCard({});
+  const validateMnemonicPressed = async () => {
+    const mnemonic = ['super', 'turtle', 'music', 'prize', 'civil', 'state', 'oxygen', 'urban', 'tank', 'valley', 'labor', 'pet'];
+    const resp = await validateMnemonic(mnemonic);
     console.log(resp);
   };
 
-  const signPressed = async () => {
-    const resp = await signMultiple({
-      signPayloads: [{
-        pubKeyBase58: '2935LwRPAuT7xsMnibfDirrtb3dD3X5WNrRDS6FeWNbVA',
-        unsignedHex: '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9',
-      }],
-      accessCode: '141414',
-    });
+  const enableUserCodeRecoveryPressed = async (enable: boolean) => {
+    const resp = await enableUserCodeRecovery({ enable, accessCode: '141414' });
     console.log(resp);
   };
 
@@ -69,10 +62,10 @@ function App(): React.JSX.Element {
       <View style={containerStyle}>
         <Button title="Scan" onPress={() => scanPressed()} />
         <Button title="Scan (Migrate)" onPress={() => scanPressed(true)} />
-        <Button title="Sign" onPress={() => signPressed()} />
-        <Button title="CreateAllWallets" onPress={() => createAllWalletsPressed()} />
         <Button title="PurgeEd25519" onPress={() => purgeAllWalletsPressed(true)} />
-        <Button title="ResetCard" onPress={() => resetCardPressed()} />
+        <Button title="validateMnemonic" onPress={() => validateMnemonicPressed()} />
+        <Button title="enableUserCodeRecovery (true)" onPress={() => enableUserCodeRecoveryPressed(true)} />
+        <Button title="enableUserCodeRecovery (false)" onPress={() => enableUserCodeRecoveryPressed(false)} />
       </View>
     </SafeAreaView>
   );
